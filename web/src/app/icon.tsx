aa337d2de0311a82b735cfb9c3c31bcd132dc291
Raphie <raphie@Raphies-MacBook-Air.local>
2026-09-12 23:31:58 +0100

    icon: strand mark becomes the favicon (consistent with the BUIDL logo)

--- web/src/app/icon.tsx
+++ web/src/app/icon.tsx
@@ -3,6 +3,8 @@ import { ImageResponse } from "next/og";
 export const size = { width: 512, height: 512 };
 export const contentType = "image/png";
 
+// The brand mark: the proof strand. Two nodes, one connection,
+// solid at the verified end. Simplified to survive 32px favicons.
 export default function Icon() {
   return new ImageResponse(
     (
@@ -10,24 +12,70 @@ export default function Icon() {
         style={{
           width: "100%",
           height: "100%",
-          background: "#0c0e10",
+          background: "#0C0E10",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          border: "12px solid #1f2428",
         }}
       >
         <div
           style={{
-            color: "#4976ff",
-            fontSize: 300,
-            fontWeight: 700,
-            fontFamily: "sans-serif",
+            width: 400,
+            height: 400,
+            borderRadius: "50%",
+            border: "10px solid #1F2428",
             display: "flex",
-            paddingBottom: 30,
+            alignItems: "center",
+            justifyContent: "center",
+            position: "relative",
           }}
         >
-          G
+          <div
+            style={{
+              width: 110,
+              height: 110,
+              borderRadius: "50%",
+              border: "14px solid #4976FF",
+              background: "#111417",
+              position: "absolute",
+              left: 8,
+              display: "flex",
+            }}
+          />
+          <div
+            style={{
+              width: 210,
+              borderTop: "14px solid #4976FF",
+              position: "absolute",
+              left: 118,
+              right: 118,
+              display: "flex",
+            }}
+          />
+          <div
+            style={{
+              width: 110,
+              height: 110,
+              borderRadius: "50%",
+              background: "#4976FF",
+              position: "absolute",
+              right: 8,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                width: 40,
+                height: 22,
+                borderBottom: "10px solid #fff",
+                borderLeft: "10px solid #fff",
+                transform: "rotate(-45deg) translate(4px, -6px)",
+                display: "flex",
+              }}
+            />
+          </div>
         </div>
       </div>
     ),
